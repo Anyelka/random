aa343cad5430a4defd5007ed63bf1bb84b8ba7d8
Marton Takacs <marton.takacs@tesco.com>
2025-03-31 10:47:14 +0200

    unique paths refact

--- src/main/kotlin/com/random/problems/leetCode/uniquePaths/UniquePaths.kt
+++ src/main/kotlin/com/random/problems/leetCode/uniquePaths/UniquePaths.kt
@@ -4,9 +4,9 @@ import com.random.util.test
 
 fun main() {
     val input = listOf<Pair<Pair<Int, Int>, Int>>(
-        /*(3 to 7) to 28,*/
+        (3 to 7) to 28,
         (3 to 2) to 3,
-        /*(23 to 12) to 193536720*/
+        (23 to 12) to 193536720
     )
 
     input.forEach {
