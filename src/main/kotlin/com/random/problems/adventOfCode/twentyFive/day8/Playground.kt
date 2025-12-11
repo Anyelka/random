045651fd9c488ff15b47f6d8f40fcc713cf2c591
Marton Takacs <marton.takacs@tesco.com>
2025-12-11 20:28:50 +0100

    aoc day 8 part 2

--- src/main/kotlin/com/random/problems/adventOfCode/twentyFive/day8/Playground.kt
+++ src/main/kotlin/com/random/problems/adventOfCode/twentyFive/day8/Playground.kt
@@ -1,23 +1,21 @@
 package com.random.problems.adventOfCode.twentyFive.day8
 
 import com.random.util.getResourceAsText
-import com.random.util.pow
 import java.util.PriorityQueue
-import kotlin.math.pow
-import kotlin.math.sqrt
 import kotlin.time.measureTime
 
 fun main() {
-    val testCases = mapOf(
-        0 to Pair("/adventOfCode/2025/day8/TestCase0",10),
-        1 to Pair("/adventOfCode/2025/day8/TestCase1",1000)
+    val testCases = listOf(
+        Pair("/adventOfCode/2025/day8/TestCase0",10),
+        Pair("/adventOfCode/2025/day8/TestCase1",1000)
     )
 
-    testCases[1]!!.let { testCase ->
+    testCases.forEach { testCase ->
         val lines = getResourceAsText(testCase.first)!!.lines()
 
         val timeTaken = measureTime {
             println("AOC day 8 - Part 1 solution: ${Part1(testCase.second).solve(parse(lines.filter { it.isNotEmpty() }))}")
+            println("AOC day 8 - Part 2 solution: ${Part2.solve(parse(lines.filter { it.isNotEmpty() }))}")
         }
         println("       -> Time taken: $timeTaken")
     }
@@ -34,54 +32,16 @@ class Point(val x: Int, val y: Int, val z: Int) {
         }
     }
 
-}
-
-class Part1(val repeat: Int) {
-    // wrong results:
-    //      72072: too low
-    ///     33696: too low
-    //      352584: good solution
-
-    fun solve(points: List<Point>): Long {
-        val minDistances = PriorityQueue<Pair<Long, Pair<Int, Int>>>(Comparator.comparing { it.first })
-
-        for(i in points.indices) {
-            for(j in i..points.lastIndex) {
-                if(i == j) continue
-                val distance = distance(points[i], points[j])
-                minDistances.offer(distance to (i to j))
-            }
-        }
-
-        val circuits = Circuits(points.size)
-
-        repeat(repeat) {
-            minDistances.poll().second.let { circuits.union(it.first, it.second) }
-        }
-
-        val circuitSizes = Array(points.size) { 0L }
-        points.indices.forEach { circuitSizes[circuits.find(it)]++ }
-
-        return circuitSizes
-            .asSequence()
-            .filter { it != 0L }
-            .sortedDescending()
-            .take(3)
-            .fold(1L) { acc, x -> acc * x }
-    }
-
-
-    fun distance(p1: Point, p2: Point): Long {
-        val dx = (p1.x - p2.x).toLong()
-        val dy = (p1.y - p2.y).toLong()
-        val dz = (p1.z - p2.z).toLong()
+    fun distanceTo(p2: Point): Long {
+        val dx = (x - p2.x).toLong()
+        val dy = (y - p2.y).toLong()
+        val dz = (z - p2.z).toLong()
         return dx*dx + dy*dy + dz*dz
     }
-
 }
 
-class Circuits(n: Int) {
-    val parents = Array(n) { it }
+class Circuits(public val size: Int) {
+    val parents = Array(size) { it }
 
     fun find(x: Int): Int {
         if(parents[x] == x) return x
@@ -98,12 +58,59 @@ class Circuits(n: Int) {
     }
 }
 
-
-/*
-object Part2 {
-    // TODO: CHECK WITH LONG!!!
-    fun solve(points: List<Point>): Int {
-        return 0
+fun  PriorityQueue<Pair<Long, Pair<Int, Int>>>.fill(points: List<Point>) {
+    for(i in points.indices) {
+        for(j in i..points.lastIndex) {
+            if(i == j) continue
+            this.offer(points[i].distanceTo(points[j]) to (i to j))
+        }
     }
 }
-*/
+
+class Part1(val repeat: Int) {
+    fun solve(points: List<Point>): Long {
+        val minDistances = PriorityQueue<Pair<Long, Pair<Int, Int>>>(Comparator.comparing { it.first })
+
+        minDistances.fill(points)
+
+        val circuits = Circuits(points.size)
+
+        repeat(repeat) {
+            minDistances.poll().second.let { circuits.union(it.first, it.second)}
+        }
+
+        val circuitSizes = Array(points.size) { 0L }
+        points.indices.forEach { circuitSizes[circuits.find(it)]++ }
+
+        return circuitSizes
+            .asSequence()
+            .filter { it != 0L }
+            .sortedDescending()
+            .take(3)
+            .fold(1L) { acc, x -> acc * x }
+    }
+
+}
+
+object Part2 {
+    fun solve(points: List<Point>): Long {
+        val minDistances = PriorityQueue<Pair<Long, Pair<Int, Int>>>(Comparator.comparing { it.first })
+
+        minDistances.fill(points)
+        val circuits = Circuits(points.size)
+
+        var firstIndex = -1
+        var secondIndex = -1
+        var circuitCount = circuits.size
+
+        while(circuitCount > 1) {
+            val pair = minDistances.poll().second
+            firstIndex = pair.first
+            secondIndex = pair.second
+            val merged = circuits.union(firstIndex, secondIndex)
+            if(merged) circuitCount--
+
+        }
+        return points[firstIndex].x.toLong() * points[secondIndex].x
+    }
+}
